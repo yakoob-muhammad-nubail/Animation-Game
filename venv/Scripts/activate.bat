db3bf78a775cf0d7f0960a043a3aeba5fc7c3366
yakoobmuhammad <yakooobnmuhammad.4@gmail.com>
2024-07-04 19:58:02 -0400

    removal of parent folder

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\nubs\Documents\GitHub_Desktop_y-m-n\Animation_Game\Animation-Game\sprite_test_animation_may\venv
+set VIRTUAL_ENV=c:\Users\nubs\Documents\GitHub_Desktop_y-m-n\Animation_Game\Animation-Game\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
