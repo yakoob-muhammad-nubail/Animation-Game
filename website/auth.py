24a3290d1fdb697cd88ea13cfb05eb78eb05ddb3
yakoobmuhammad <yakooobnmuhammad.4@gmail.com>
2024-07-09 00:05:16 -0400

    localhost setup

---
+++ website/auth.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
+
+@auth.route('/home')
+def home():
+    return "<p>home</p>"
+
+@auth.route('/game')
+def game():
+    return "<p>home</p>"
---
+++ website/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+
+views = Blueprint('views', __name__)
+
+@views.route('/login')
+def login():
+    return "<p>login</p>"
+
+@views.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
+@views.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
+
+@views.route('/home')
+def home():
+    return "<p>home</p>"
+
+@views.route('/game')
+def game():
+    return "<p>home</p>"
